636ddf0c4da6f5b3af53a88296ba53d472d54287
李奇 <2278962296@qq.com>
2025-06-23 17:17:17 +0800

    版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 9
-        versionName = "1.0.9"
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
