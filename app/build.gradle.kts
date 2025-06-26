63009ec2061601ccbec861dc9a37c9f66eedc935
李奇 <2278962296@qq.com>
2025-06-26 12:05:45 +0800

    修复小分辨率手机布局问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.vaytree.antic"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
